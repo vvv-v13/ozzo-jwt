7120b0a7442a8bed7142e422764bb04f87d35e2b
vvv-v13 <vetal13@gmail.com>
2016-03-06 22:06:07 +0200

    Fix license header

--- handlers_test.go
+++ handlers_test.go
@@ -1,4 +1,6 @@
-// Copyright 2016 Qiang Xue. All rights reserved.
+// Copyright (c) 2016, Vitalii Panko <vetal13@gmail.com>
+// All rights reserved.
+//
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
