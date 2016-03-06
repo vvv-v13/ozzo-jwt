24b07304fabc2584defead2b2babdb747f3b6708
vvv-v13 <vetal13@gmail.com>
2016-03-06 22:00:35 +0200

    100% cover

--- handlers_test.go
+++ handlers_test.go
@@ -5,32 +5,31 @@
 package jwt
 
 import (
-	"errors"
+        "errors"
 	"github.com/go-ozzo/ozzo-routing"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-        "time"
+	"time"
 )
-
-
 
 func identity(c *routing.Context, payload JWTPayload) (Payload, error) {
         if id, ok := payload["id"]; ok {
                 return Payload(id), nil
         }
         return nil, errors.New("invalid credential")
-}
 
+        
+}
 
 func TestJWT(t *testing.T) {
 
-        jwtConfig := JWTConfig{
-                Alg:     "HS256",
-                Secret:  "super_secret",
-                Expires: time.Now().Add(time.Minute * 120).Unix(),
-        }
+	jwtConfig := JWTConfig{
+		Alg:     "HS256",
+		Secret:  "super_secret",
+		Expires: time.Now().Add(time.Minute * 120).Unix(),
+	}
 
 	h := JWT(identity, jwtConfig, "App")
 
@@ -43,20 +42,32 @@ func TestJWT(t *testing.T) {
 	}
 	assert.Equal(t, `Bearer realm="App"`, res.Header().Get("WWW-Authenticate"))
 	assert.Nil(t, c.Get(User))
-        
-        payload := make(JWTPayload)
-        payload["id"] = "user_id"
 
-        token, err := CreateToken(jwtConfig, payload)
-        
+	payload := make(JWTPayload)
+	payload["id"] = "user_id"
+
+	token, err := CreateToken(jwtConfig, payload)
+
 	req, _ = http.NewRequest("GET", "/demo/", nil)
-	req.Header.Set("Authorization", "Bearer " + token)
+	req.Header.Set("Authorization", "Bearer "+token)
 	res = httptest.NewRecorder()
 	c = routing.NewContext(res, req)
 	err = h(c)
 	assert.Nil(t, err)
 	assert.Equal(t, "", res.Header().Get("WWW-Authenticate"))
 	assert.Equal(t, "user_id", c.Get(User))
+
+        payload = make(JWTPayload)
+        payload["uid"] = "user_id"
+        token, err = CreateToken(jwtConfig, payload)
+
+        req, _ = http.NewRequest("GET", "/demo/", nil)
+        req.Header.Set("Authorization", "Bearer "+token)
+        res = httptest.NewRecorder()
+        c = routing.NewContext(res, req)
+        err = h(c)
+        assert.NotNil(t, err)
+        assert.Nil(t, c.Get(User))
 
 	req, _ = http.NewRequest("GET", "/demo/", nil)
 	req.Header.Set("Authorization", "Bearer QW")
@@ -70,3 +81,36 @@ func TestJWT(t *testing.T) {
 	assert.Nil(t, c.Get(User))
 }
 
+func TestCreateToken(t *testing.T) {
+        payload := make(JWTPayload)
+        payload["id"] = "user_id"
+
+        jwtConfig := JWTConfig{
+                Alg:     "HS256",
+                Secret:  "super_secret",
+                Expires: time.Now().Add(time.Minute * 120).Unix(),
+        }
+
+	token, err := CreateToken(jwtConfig, payload)
+	assert.Nil(t, err)
+        assert.NotNil(t, token)
+
+        jwtConfig = JWTConfig{
+                Alg:     "HS384",
+                Secret:  "super_secret",
+                Expires: time.Now().Add(time.Minute * 120).Unix(),
+        }
+
+        token, err = CreateToken(jwtConfig, payload)
+        assert.Nil(t, err)
+        assert.NotNil(t, token)
+
+        jwtConfig = JWTConfig{
+                Alg:     "HS512",
+                Secret:  "super_secret",
+                Expires: time.Now().Add(time.Minute * 120).Unix(),
+        }
+        token, err = CreateToken(jwtConfig, payload)
+        assert.Nil(t, err)
+        assert.NotNil(t, token)
+}
