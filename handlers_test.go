e169713d8581f3194a10a18921e7a8452d5071a1
vvv-v13 <vetal13@gmail.com>
2016-03-06 21:29:39 +0200

    Tests

---
+++ handlers_test.go
@@ -0,0 +1,72 @@
+// Copyright 2016 Qiang Xue. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package jwt
+
+import (
+	"errors"
+	"github.com/go-ozzo/ozzo-routing"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+        "time"
+)
+
+
+
+func identity(c *routing.Context, payload JWTPayload) (Payload, error) {
+        if id, ok := payload["id"]; ok {
+                return Payload(id), nil
+        }
+        return nil, errors.New("invalid credential")
+}
+
+
+func TestJWT(t *testing.T) {
+
+        jwtConfig := JWTConfig{
+                Alg:     "HS256",
+                Secret:  "super_secret",
+                Expires: time.Now().Add(time.Minute * 120).Unix(),
+        }
+
+	h := JWT(identity, jwtConfig, "App")
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/users/", nil)
+	c := routing.NewContext(res, req)
+	err := h(c)
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "invalid credential", err.Error())
+	}
+	assert.Equal(t, `Bearer realm="App"`, res.Header().Get("WWW-Authenticate"))
+	assert.Nil(t, c.Get(User))
+        
+        payload := make(JWTPayload)
+        payload["id"] = "user_id"
+
+        token, err := CreateToken(jwtConfig, payload)
+        
+	req, _ = http.NewRequest("GET", "/demo/", nil)
+	req.Header.Set("Authorization", "Bearer " + token)
+	res = httptest.NewRecorder()
+	c = routing.NewContext(res, req)
+	err = h(c)
+	assert.Nil(t, err)
+	assert.Equal(t, "", res.Header().Get("WWW-Authenticate"))
+	assert.Equal(t, "user_id", c.Get(User))
+
+	req, _ = http.NewRequest("GET", "/demo/", nil)
+	req.Header.Set("Authorization", "Bearer QW")
+	res = httptest.NewRecorder()
+	c = routing.NewContext(res, req)
+	err = h(c)
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "token contains an invalid number of segments", err.Error())
+	}
+	assert.Equal(t, `Bearer realm="App"`, res.Header().Get("WWW-Authenticate"))
+	assert.Nil(t, c.Get(User))
+}
+
